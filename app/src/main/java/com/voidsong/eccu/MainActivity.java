cd6c50b0cc5d8ae8d03752f14fb0ade211796cf9
Sinketsu <ssolloss@yandex.ru>
2017-04-25 21:31:22 +0500

    create constants for MainActivity layout

--- app/src/main/java/com/voidsong/eccu/MainActivity.java
+++ app/src/main/java/com/voidsong/eccu/MainActivity.java
@@ -2,7 +2,6 @@ package com.voidsong.eccu;
 
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
