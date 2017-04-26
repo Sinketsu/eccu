51208583b7f8dbd70cd54cf7ba1b8251a5337f17
reconrus <reconrus@yandex.ru>
2017-04-26 15:43:25 +0500

    Classes added

--- app/src/main/java/com/voidsong/eccu/DoorDialog.java
+++ app/src/main/java/com/voidsong/eccu/DoorDialog.java
@@ -2,36 +2,25 @@ package com.voidsong.eccu;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ToggleButton;
 
 /**
- * Created by CoolHawk on 4/23/2017.
+ * Created by CoolHawk on 4/26/2017.
  */
 
-public class BulbDialog extends DialogFragment {
+public class DoorDialog extends DialogFragment {
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        final View main_view = inflater.inflate(R.layout.dialog_bulb, null);
+        final View main_view = inflater.inflate(R.layout.dialog_door, null);
         builder.setView(main_view);
         return builder.create();
     }
-
-
-    /*@Override
-    public void onCancel(DialogInterface dialog) {
-        //Здеся надо сделать его положительным ответом
-        super.onCancel(dialog);
-    }*/
-
-
 }
