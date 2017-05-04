6d9fff6a89f7d8636a7d7b73dc09cfc205c5fb37
Sinketsu <ssolloss@yandex.ru>
2017-05-04 20:37:13 +0500

    try to create doorDialog gui w/o SwitchIcon

--- app/src/main/java/com/voidsong/eccu/dialogs/DoorDialog.java
+++ app/src/main/java/com/voidsong/eccu/dialogs/DoorDialog.java
@@ -2,13 +2,18 @@ package com.voidsong.eccu.dialogs;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 //import com.github.zagum.switchicon.SwitchIconView;
 import com.voidsong.eccu.R;
@@ -31,50 +36,83 @@ public class DoorDialog extends DialogFragment {
         void setOpenedDoorCount(Integer opened, Integer all);
     }
 
+    private class Door {
+        private int _id;
+        private LinearLayout _control;
+        private boolean _state;
+
+        public void setControl(LinearLayout control) {
+            _control = control;
+            _control.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // SwitchIcon.toggle();
+                    if (_control.getOrientation() == LinearLayout.HORIZONTAL) {
+                        _control.setOrientation(LinearLayout.VERTICAL);
+                        _state = true;
+                        _control.setBackgroundColor(ContextCompat.getColor(getActivity().getApplicationContext(), R.color.colorRed));
+                    } else {
+                        _control.setOrientation(LinearLayout.HORIZONTAL);
+                        _state = false;
+                        _control.setBackgroundColor(ContextCompat.getColor(getActivity().getApplicationContext(), R.color.colorCyan));
+                    }
+
+                    // request to server
+                }
+            });
+        }
+        public void verifyState() {
+            if (_state) {
+                _control.setOrientation(LinearLayout.VERTICAL);
+                _control.setBackgroundColor(ContextCompat.getColor(getActivity().getApplicationContext(), R.color.colorRed));
+            } else {
+                _control.setOrientation(LinearLayout.HORIZONTAL);
+                _control.setBackgroundColor(ContextCompat.getColor(getActivity().getApplicationContext(), R.color.colorCyan));
+            }
+        }
+        public void setState(boolean state) {
+            _state = state;
+        }
+        public boolean getState() {
+            return _state;
+        }
+    }
+
+    private Door door;
+
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                .setPositiveButton(getResources().getString(R.string.ok),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
         LayoutInflater inflater = getActivity().getLayoutInflater();
         final View main_view = inflater.inflate(R.layout.dialog_door, null);
-        final Button btn = (Button)main_view.findViewById(R.id.btn2);
-        //final SwitchIconView d = (SwitchIconView)main_view.findViewById(R.id.SW);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /*
-                OkHttpClient client = Internet.getClient();
-                Request request = new Request.Builder()
-                        .url("https://" + Settings.getIp() + "/api/get/light")
-                        .build();
-                client.newCall(request).enqueue(new Callback() {
-                    @Override
-                    public void onFailure(Call call, IOException e) {
-                        btn.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                btn.setText("FAIL");
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onResponse(Call call, final Response response) throws IOException {
-                        final String g = response.body().string();
-                        Log.d("TAGMYTAG", g);
-                        btn.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                btn.setText(g);
-                            }
-                        });
-                    }
-                });*/
-                //d.switchState();
-            }
-        });
+        if (door == null) {
+            door = new Door();
+            door.setControl((LinearLayout) main_view.findViewById(R.id.door));
+        } else {
+            door.setControl((LinearLayout) main_view.findViewById(R.id.door));
+        }
         builder.setView(main_view);
-
         return builder.create();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        door.verifyState();
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+        IDoorController activity = (IDoorController) getActivity();
+        activity.setOpenedDoorCount(door.getState() ? 1 : 0, 1);
     }
 }
